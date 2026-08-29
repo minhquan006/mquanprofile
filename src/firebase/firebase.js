7dfa74cde55445be6da742f441ddafed5254258a
Minh Quan <quannguyen2342006@gmail.com>
2026-08-29 22:59:47 +0700

    Setup Firebase for views and likes

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,12 +1,13 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "...CỦA BẠN...",
-  authDomain: "your-project.firebaseapp.com",
-  projectId: "your-project",
-  storageBucket: "your-project.firebasestorage.app",
-  messagingSenderId: "...",
-  appId: "..."
+  apiKey: "AIzaSyCGWnyqNZKJ82XcmrxmrWhVPG7-jqU1XCE",
+  authDomain: "mquan-profile.firebaseapp.com",
+  databaseURL: "https://mquan-profile-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "mquan-profile",
+  storageBucket: "mquan-profile.firebasestorage.app",
+  messagingSenderId: "1022414717838",
+  appId: "1:1022414717838:web:ac7f6e01304806e7f8dadf"
 };
 
 export const app = initializeApp(firebaseConfig);
